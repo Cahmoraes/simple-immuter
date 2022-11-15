9ea880f9e006a3203499ea2ad50d772d7a63924f
caique.moraes <caique.moraes@compasso.com.br>
2022-11-15 09:26:59 -0300

    build do projeto

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,6 +1,9 @@
 declare const _default: {
-    produce: <T>(baseState: T | Promise<T>, producer?: ((draft: T) => void) | undefined) => any;
-    merge: <T_1, K extends object[]>(baseState: T_1 | Promise<T_1>, ...states: K) => T_1 & K;
+    produce: <T extends object | any[] | Map<any, any> | Set<any>>(baseState: T | Promise<T>, producer?: (object | ((draftState: T & {
+        [key: string]: any;
+    }) => any)) | undefined, ...states: any[]) => any;
+    deepClone: <T_1 extends object | any[] | Map<any, any> | Set<any>>(element: T_1) => T_1;
+    deepFreeze: <T_2 extends object | any[] | Map<any, any> | Set<any>>(elementToFreeze: T_2) => T_2;
 };
 
 export { _default as si };
