19379eb86ad2e029f519dff670490adea1e41032
caique.moraes <caique.moraes@compasso.com.br>
2022-11-16 10:17:58 -0300

    Build do projeto

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,9 +1,9 @@
 declare const _default: {
     produce: {
-        <T extends object | any[] | Map<any, any> | Set<any>>(baseState: T & {
+        <T extends object | Map<unknown, unknown> | Set<unknown> | unknown[]>(baseState: T & {
             [key: string]: any;
         }): T;
-        <T_1 extends object | any[] | Map<any, any> | Set<any>>(baseState: T_1 & {
+        <T_1 extends object | Map<unknown, unknown> | Set<unknown> | unknown[]>(baseState: T_1 & {
             [key: string]: any;
         }, producer: (draftState: T_1 & {
             [key: string]: any;
@@ -13,8 +13,8 @@ declare const _default: {
             [key: string]: any;
         };
     };
-    deepClone: <T_2 extends object | any[] | Map<any, any> | Set<any>>(element: T_2) => T_2;
-    deepFreeze: <T_3 extends object | any[] | Map<any, any> | Set<any>>(elementToFreeze: T_3) => T_3;
+    deepClone: <T_2 extends object | Map<unknown, unknown> | Set<unknown> | unknown[]>(element: T_2) => T_2;
+    deepFreeze: <T_3 extends object | Map<unknown, unknown> | Set<unknown> | unknown[]>(elementToFreeze: T_3) => T_3;
 };
 
 export { _default as si };
