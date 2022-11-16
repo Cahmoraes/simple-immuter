6f2d12d8e5e25a7564fbf1891790bacb8e948ea5
caique.moraes <caique.moraes@compasso.com.br>
2022-11-16 14:54:00 -0300

    build do projeto

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -2,19 +2,19 @@ declare const _default: {
     produce: {
         <T extends object | Map<unknown, unknown> | Set<unknown> | unknown[]>(baseState: T & {
             [key: string]: any;
-        }): T;
+        }): Readonly<T>;
         <T_1 extends object | Map<unknown, unknown> | Set<unknown> | unknown[]>(baseState: T_1 & {
             [key: string]: any;
         }, producer: (draftState: T_1 & {
             [key: string]: any;
         }) => void | (T_1 & {
             [key: string]: any;
-        })): T_1 & {
+        })): Readonly<T_1> & {
             [key: string]: any;
         };
     };
     deepClone: <T_2 extends object | Map<unknown, unknown> | Set<unknown> | unknown[]>(element: T_2) => T_2;
-    deepFreeze: <T_3 extends object | Map<unknown, unknown> | Set<unknown> | unknown[]>(elementToFreeze: T_3) => T_3;
+    deepFreeze: <T_3 extends object | Map<unknown, unknown> | Set<unknown> | unknown[]>(elementToFreeze: T_3) => Readonly<T_3>;
 };
 
 export { _default as si };
