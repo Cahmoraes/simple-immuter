4fe22fabf127033164ca19a95610aac7736f44b8
caique.moraes <ike.moraes93@gmail.com>
2022-12-02 20:52:57 -0300

    build do projeto

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,18 +1,13 @@
 declare const _default: {
     produce: {
-        <T extends object | Map<unknown, unknown> | Set<unknown> | unknown[] | Date>(baseState: T & {
-            [key: string]: any;
-        }): Readonly<T>;
-        <T_1 extends object | Map<unknown, unknown> | Set<unknown> | unknown[] | Date>(baseState: T_1 & {
-            [key: string]: any;
-        }, producer: (draftState: T_1 & {
-            [key: string]: any;
-        }) => void | (T_1 & {
-            [key: string]: any;
-        })): Readonly<T_1>;
+        <T extends object | Map<unknown, unknown> | Set<unknown> | unknown[] | Date>(baseState: T): Readonly<T>;
+        <T_1 extends object | Map<unknown, unknown> | Set<unknown> | unknown[] | Date>(baseState: T_1, producer: (draftState: T_1) => void | T_1, config: {
+            freeze: boolean;
+        }): Readonly<T_1>;
+        <T_2 extends object | Map<unknown, unknown> | Set<unknown> | unknown[] | Date>(baseState: T_2, producer: (draftState: T_2) => void | T_2): Readonly<T_2>;
     };
-    cloneDeep: <T_2 extends object | Map<unknown, unknown> | Set<unknown> | unknown[] | Date>(anElement: T_2) => T_2;
-    freezeDeep: <T_3 extends object | Map<unknown, unknown> | Set<unknown> | unknown[] | Date>(elementToFreeze: T_3) => Readonly<T_3>;
+    cloneDeep: <T_3 extends object | Map<unknown, unknown> | Set<unknown> | unknown[] | Date>(anElement: T_3) => T_3;
+    freezeDeep: <T_4 extends object | Map<unknown, unknown> | Set<unknown> | unknown[] | Date>(elementToFreeze: T_4) => Readonly<T_4>;
 };
 
 export { _default as si };
