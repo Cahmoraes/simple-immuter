f68e93dd0ddbf50ed543eb09fd0d1797fb5667c4
caique.moraes <ike.moraes93@gmail.com>
2022-12-02 15:49:06 -0300

    build do projeto

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -9,7 +9,7 @@ declare const _default: {
             [key: string]: any;
         }) => void | (T_1 & {
             [key: string]: any;
-        })): Readonly<T_1> & Record<string, unknown>;
+        })): Readonly<T_1>;
     };
     cloneDeep: <T_2 extends object | Map<unknown, unknown> | Set<unknown> | unknown[] | Date>(anElement: T_2) => T_2;
     freezeDeep: <T_3 extends object | Map<unknown, unknown> | Set<unknown> | unknown[] | Date>(elementToFreeze: T_3) => Readonly<T_3>;
