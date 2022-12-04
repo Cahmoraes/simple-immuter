8d0c28cca3d156bae892f60c3cd9832fa918ca13
caique.moraes <ike.moraes93@gmail.com>
2022-12-04 20:21:15 -0300

    build do projeto

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -3,8 +3,8 @@ declare const _default: {
         <T extends object | Map<unknown, unknown> | Set<unknown> | unknown[] | Date>(baseState: T): Readonly<T>;
         <T_1 extends object | Map<unknown, unknown> | Set<unknown> | unknown[] | Date>(baseState: T_1, producer: (draftState: T_1) => void | T_1, config: {
             freeze: boolean;
-        }): Readonly<T_1>;
-        <T_2 extends object | Map<unknown, unknown> | Set<unknown> | unknown[] | Date>(baseState: T_2, producer: (draftState: T_2) => void | T_2): Readonly<T_2>;
+        }): Readonly<Readonly<T_1>>;
+        <T_2 extends object | Map<unknown, unknown> | Set<unknown> | unknown[] | Date>(baseState: T_2, producer: (draftState: T_2) => void | T_2): Readonly<Readonly<T_2>>;
     };
     cloneDeep: <T_3 extends object | Map<unknown, unknown> | Set<unknown> | unknown[] | Date>(anElement: T_3) => T_3;
     freezeDeep: <T_4 extends object | Map<unknown, unknown> | Set<unknown> | unknown[] | Date>(elementToFreeze: T_4) => Readonly<T_4>;
